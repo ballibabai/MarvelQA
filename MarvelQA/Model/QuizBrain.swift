b907229b65e8d4b086b217250cfe704ed24ca0c7
ballibabai <w4nderlust3rr@gmail.com>
2022-05-10 02:19:09 +0300

    // delete mutating from func getScore

--- MarvelQA/Model/QuizBrain.swift
+++ MarvelQA/Model/QuizBrain.swift
@@ -49,7 +49,7 @@ struct QuizBrain {
         return Float(questionNumber) / Float(quiz.count)
     }
     
-    mutating func getScore() -> Int{
+     func getScore() -> Int{
         return score
     }
     
